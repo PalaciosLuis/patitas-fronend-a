dba7c9b480c1f1863f008a44601543c40f9dbc4c
Luis Javier <924333523javier@gmail.com>
2024-09-17 20:30:03 -0500

    first commit

---
+++ src/main/java/pe/edu/cibertec/patita_fronend_a/PatitaFronendAApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patita_fronend_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitaFronendAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitaFronendAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/patita_fronend_a/PatitaFronendAApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patita_fronend_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitaFronendAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
